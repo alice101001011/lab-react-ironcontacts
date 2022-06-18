aa6848055e5144f926fa6bdfa2d4aafcaa5ebdd2
alice101001011 <alice101001011@gmail.com>
2022-06-18 14:37:38 +0200

    lab done (except styling)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contactsList from "./contacts.json";
+import { useState } from "react";
 
 function App() {
+  const [contacts, setContacts] = useState(contactsList.slice(0, 5));
+
+  const randomButton = () => {
+    let randomContact = Math.floor(Math.random() * contactsList.length);
+    let contactsCopy = [...contacts];
+    contactsCopy.push(contactsList[randomContact]);
+    setContacts(contactsCopy);
+  };
+
+  function handleSortByPopularity() {
+    let contactsCopy = [...contacts];
+    let contactsByPopularity = contactsCopy.sort((a, b) => {
+      return b.popularity - a.popularity;
+    });
+    setContacts(contactsByPopularity);
+  }
+
+  function handleSortByName() {
+    let contactsCopy = [...contacts];
+    let contactsByName = contactsCopy.sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    setContacts(contactsByName);
+  }
+
+  const deleteContact = (contactId) => {
+    const filteredContact = contacts.filter((contact) => {
+      return contact.id !== contactId;
+    });
+    setContacts(filteredContact);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Iron Contacts</h1>
+      <div className="buttons">
+        <div className="btn-group">
+          <button
+            className="btn btn-outline-dark"
+            onClick={() => handleSortByPopularity()}>
+            Sort by popularity
+          </button>
+          <button
+            className="btn btn-outline-dark"
+            onClick={() => handleSortByName()}>
+            Sort by name
+          </button>
+        </div>
+        <button className="btn btn-outline-dark" onClick={randomButton}>
+          Add random Contact
+        </button>
+      </div>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td>
+                <img
+                  className="contact-image"
+                  src={contact.pictureUrl}
+                  alt={contact.name}
+                />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+              <td>{contact.wonOscar ? <p>🏆</p> : <p></p>}</td>
+              <td>{contact.wonEmmy ? <p>🏅</p> : <p></p>}</td>
+              <button
+                onClick={() => deleteContact(contact.id)}
+                className="btn-delete">
+                Delete
+              </button>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
